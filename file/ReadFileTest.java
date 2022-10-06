6fc56e940167b5542105e7067759c1163318996c
Win10ft\UserK <dnwndhks98@gmail.com>
2022-10-06 15:13:32 +0900

    first commit  현재 디렉토리 파일 읽기

---
+++ file/ReadFileTest.java
@@ -0,0 +1,29 @@
+package java10M.week1.day5.file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileTest {
+    public static void main(String[] args) throws FileNotFoundException { // throws FileNotFoundException // 파일이 없을 시 예외처리
+        //      ./ : 현재 디랙토리
+        //  프로젝트 루트 : 인텔리제이 가장 상단에 있는 디렉토리 java
+
+        // 현재 디렉토리에 첫번째 파일 읽기
+        FileReader fileReader =new FileReader("./a_file.txt");
+        File dir = new File("./");
+        File files[] = dir.listFiles();
+        System.out.println(files[0]);
+        for (File file : files) {
+            System.out.println(file);
+        }
+
+//        ReadFile readFile = new ReadFile("a_file.txt");
+//        try {
+//            System.out.println(readFile.readAByte());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
